fd52342ede86c6bb299a2e2a26a66dfb39cc1e8d
ElenaMutykova <elena.mutykova@virtoway.com>
2024-09-28 18:14:11 +0200

    allocation of the helper class GroupHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.by import By
+from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
 
@@ -10,34 +10,11 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_group_page()
-        # init group creation
-        wd.find_element(By.NAME, "new").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        # fill group form
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element(By.NAME, "submit").click()
-        self.return_to_group_page()
-
-    def return_to_group_page(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "group page").click()
 
     def destroy(self):
         self.wd.quit()
