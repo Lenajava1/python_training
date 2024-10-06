8b015a2a8a5d33b61ddefb5b06d5ab1ec4e63460
ElenaMutykova <elena.mutykova@virtoway.com>
2024-10-06 17:40:23 +0200

    Login-logout. Validation of current session state

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,5 +4,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="test_new", header="test", footer="test"))
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
