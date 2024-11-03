58123194c7dac5058ebfe4075f3ee60dc2b2480c
ElenaMutykova <elena.mutykova@virtoway.com>
2024-11-03 15:10:15 +0100

    Task18: Implement test data generators for contacts. Task19: Implement loading test data from files

---
+++ generator/contact.py
@@ -0,0 +1,57 @@
+import jsonpickle
+from model.contact import Contact
+import random
+import string
+import os.path
+import getopt
+import sys
+
+
+def print_usage():
+    pass
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    print(err)
+    print_usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+        if o == "-n":
+            n = int(a)
+        elif o == "-f":
+            f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", middlename="", nickname="", title="", company_name="", address="", homephone="", workphone="", fax="", email="")] + [
+Contact(
+    firstname=random_string("firstname", 6),
+    lastname=random_string("lastname", 10),
+    middlename=random_string("middlename", 8),
+    nickname=random_string("nickname", 3),
+    title=random_string("title", 8),
+    company_name=random_string("company", 16),
+    address=random_string("address", 20),
+    mobile=random_string("", 10),
+    homephone=random_string("", 10),
+    workphone=random_string("", 12),
+    email=random_string("", 8))
+
+for i in range(n)
+
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,36 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from model.contact import Contact
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", middlename="", nickname="", title="", company_name="", address="", homephone="", workphone="", fax="", email="")] + [
-Contact(firstname=random_string("firstname", 5),
-        lastname=random_string("lastname", 10),
-        middlename=random_string("middlename", 8),
-        nickname=random_string("nickname", 3),
-        title=random_string("title", 8),
-        company_name=random_string("company", 16),
-        address=random_string("address", 20),
-        mobile=random_string("", 10),
-        homephone=random_string("", 10),
-        workphone=random_string("", 12),
-        email=random_string("", 8))
-
-for i in range(5)
-
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
-    #if app.contact.count() == 0:
-       #app.contact.create(contact)
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
